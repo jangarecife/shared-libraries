f36c9c1461b941c80de24d20b825f40a82190f11
JangeRecife <jangarecife@gmail.com>
2017-12-06 15:20:37 +0100

    ddd

--- src/org/skat/SharedClass.groovy
+++ src/org/skat/SharedClass.groovy
@@ -6,6 +6,6 @@ class SharedClass {
 	}
 	
 	def checkOut(repo) {
-		git url: "git@github.com:${repo}"
+		git credentialsId: '258ed905-321d-4293-aac8-85858f77819d', url: "git@github.com:${repo}"
     }
 }
