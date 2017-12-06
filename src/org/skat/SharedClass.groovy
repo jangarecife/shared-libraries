70e85739cda2d32d8e5b019c95b5702f5be641f5
JangeRecife <jangarecife@gmail.com>
2017-12-06 15:28:14 +0100

    ffff

--- src/org/skat/SharedClass.groovy
+++ src/org/skat/SharedClass.groovy
@@ -9,4 +9,8 @@ class SharedClass {
 		echo ${repo}
 		//git credentialsId: '258ed905-321d-4293-aac8-85858f77819d', url: "git@github.com:${repo}"
     }
+	
+	def checkOutFrom() {
+		git url: "git@github.com:jangarecife/jenkins-workshop.git"
+	  }
 }
