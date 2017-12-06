d9ee469b24bead450254bb27ac9cef1c4a3dd25a
JangeRecife <jangarecife@gmail.com>
2017-12-06 15:43:23 +0100

    hhh

--- src/org/skat/SharedClass.groovy
+++ src/org/skat/SharedClass.groovy
@@ -10,6 +10,10 @@ class SharedClass {
 		//git credentialsId: '258ed905-321d-4293-aac8-85858f77819d', url: "git@github.com:${repo}"
     }
 	
+	def getShortCommit(commit = "HEAD") {
+		return getShell().pipe("git rev-parse ${commit}").substring(0,6)
+	}
+	
 	def checkOutFrom() {
 		git url: "git@github.com:jangarecife/jenkins-workshop.git, credentialsId: '258ed905-321d-4293-aac8-85858f77819d'"
 	}
