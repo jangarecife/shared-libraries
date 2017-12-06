fbc0d1d071cee747d0f1762e0107bffc3000b728
JangeRecife <jangarecife@gmail.com>
2017-12-06 17:19:13 +0100

    Outside class scope

--- src/org/skat/SharedClass.groovy
+++ src/org/skat/SharedClass.groovy
@@ -1,19 +1,19 @@
 package org.skat
 
-class SharedClass {
-	def myName() {
-		return "Poul" 
-	}
-	
-	def checkOut(repo) {
-		git credentialsId: '258ed905-321d-4293-aac8-85858f77819d', url: "git@github.com:${repo}"
-    }
-	
+def checkOut(repo) {
+	git credentialsId: '258ed905-321d-4293-aac8-85858f77819d', url: "git@github.com:${repo}"
+}
 //	def getShortCommit(commit = "HEAD") {
 //		return getShell().pipe("git rev-parse ${commit}").substring(0,6)
 //	}
-	
-	def checkOutFrom() {
-		git url: "git@github.com:jangarecife/jenkins-workshop.git, credentialsId: '258ed905-321d-4293-aac8-85858f77819d'"
+
+//def checkOutFrom() {
+//	git url: "git@github.com:jangarecife/jenkins-workshop.git, credentialsId: '258ed905-321d-4293-aac8-85858f77819d'"
+//}
+class SharedClass {
+	def myName() {
+		return "Poul"
 	}
+
+
 }
